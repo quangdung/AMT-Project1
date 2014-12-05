37455dad41b1a2f2c1861ba91918e8f1ffa19b31
Drezaroth <drezaroth@gmail.com>
2014-12-05 17:58:53 +0100

    first test get sensors working as intended
    Conflicts:
    	AMT_Project/AMT_Project/src/main/java/ch/heigvd/amt_project/model/Fact.java
    	AMT_Project/AMT_Project/src/main/java/ch/heigvd/amt_project/model/Observation.java
    	AMT_Project/AMT_Project/src/main/java/ch/heigvd/amt_project/model/Organization.java
    	AMT_Project/AMT_Project/src/main/java/ch/heigvd/amt_project/model/User.java

---
+++ AMT_Project/AMT_Project/target/generated-sources/annotations/ch/heigvd/amt_project/model/Sensor_.java
@@ -0,0 +1,18 @@
+package ch.heigvd.amt_project.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-12-04T18:52:29")
+@StaticMetamodel(Sensor.class)
+public class Sensor_ { 
+
+    public static volatile SingularAttribute<Sensor, Long> id;
+    public static volatile SingularAttribute<Sensor, Boolean> publicSensor;
+    public static volatile SingularAttribute<Sensor, String> description;
+    public static volatile SingularAttribute<Sensor, String> name;
+    public static volatile SingularAttribute<Sensor, Long> organizationId;
+    public static volatile SingularAttribute<Sensor, String> type;
+
+}
